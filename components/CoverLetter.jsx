d1a68fa01c34269b04d81c6bbfaf6219a1b6d8f3
Sofa Radkova <sofya.radkova@duke.edu>
2024-07-13 21:18:00 -0700

    fix saves

--- components/CoverLetter.jsx
+++ components/CoverLetter.jsx
@@ -1,13 +1,13 @@
-import React, { useRef } from 'react';
-import { motion, useInView } from 'framer-motion';
+import React, { useRef } from "react";
+import { motion } from "framer-motion";
 import {
   Drawer,
   IconButton,
   ThemeProvider,
   Typography,
-} from '@material-tailwind/react';
-import Link from 'next/link';
-import { TitleText } from './CustomTexts';
+} from "@material-tailwind/react";
+import Link from "next/link";
+import { TitleText } from "./CustomTexts";
 
 export const CoverLetter = () => {
   const ref = useRef(null);
@@ -22,37 +22,37 @@ export const CoverLetter = () => {
       defaultProps: {
         size: 650,
         overlay: true,
-        placement: 'bottom',
+        placement: "bottom",
         overlayProps: undefined,
-        className: '',
+        className: "",
         dismiss: { enabled: false },
         onClose: undefined,
         transition: {
-          type: 'tween',
+          type: "tween",
           duration: 0.5,
         },
       },
       styles: {
         base: {
           drawer: {
-            position: 'fixed',
-            zIndex: 'z-[9999]',
-            pointerEvents: 'pointer-events-auto',
-            backgroundColor: 'bg-transparent',
-            boxSizing: 'box-border',
-            width: 'w-full',
-            boxShadow: 'shadow-2xl shadow-blue-gray-900/10',
+            position: "fixed",
+            zIndex: "z-[9999]",
+            pointerEvents: "pointer-events-auto",
+            backgroundColor: "bg-transparent",
+            boxSizing: "box-border",
+            width: "w-full",
+            boxShadow: "shadow-2xl shadow-blue-gray-900/10",
           },
           overlay: {
-            position: 'absolute',
-            inset: 'inset-0',
-            width: 'w-full',
-            height: 'h-full',
-            pointerEvents: 'pointer-events-auto',
-            zIndex: 'z-[9995]',
-            backgroundColor: 'bg-black',
-            backgroundOpacity: 'bg-opacity-60',
-            backdropBlur: 'backdrop-blur-sm',
+            position: "absolute",
+            inset: "inset-0",
+            width: "w-full",
+            height: "h-full",
+            pointerEvents: "pointer-events-auto",
+            zIndex: "z-[9995]",
+            backgroundColor: "bg-black",
+            backgroundOpacity: "bg-opacity-60",
+            backdropBlur: "backdrop-blur-sm",
           },
         },
       },
@@ -62,13 +62,20 @@ export const CoverLetter = () => {
   return (
     <>
       <div className="flex flex-col justify-center items-center">
-        <img src="cover-letter-title.png" alt="Cover Letter Heading" className="w-[600px] mt-[-130px] object-fill" />
+        <img
+          src="cover-letter-title.png"
+          alt="Cover Letter Heading"
+          className="w-[600px] mt-[-130px] object-fill"
+        />
         <motion.div
           whileHover={{ scale: 1.2 }}
           transition={{ duration: 0.2 }}
           className="mt-[80px]"
         >
-          <a onClick={openDrawerBottom} className="bg-transparent flex items-center justify-center cursor-pointer mt-[-180px]">
+          <a
+            onClick={openDrawerBottom}
+            className="bg-transparent flex items-center justify-center cursor-pointer mt-[-180px]"
+          >
             <img src="letter-icon.png" className="object-fill w-20" />
           </a>
         </motion.div>
@@ -91,15 +98,24 @@ export const CoverLetter = () => {
             }}
             className="flex flex-col justify-center items-center mt-[-30px]"
           >
-            <TitleText title={<>Cover letters suck</>} textStyles="text-center" />
-            <Typography className="text-[20px] text-main-black text-center mb-[8px]">Let's get to the point</Typography>
+            <TitleText
+              title={<>Cover letters suck</>}
+              textStyles="text-center"
+            />
+            <Typography className="text-[20px] text-main-black text-center mb-[8px]">
+              Let's get to the point
+            </Typography>
             <Link href="/intro">
               <IconButton className="rounded-full bg-main-black hover:shadow-[#333333]/20 focus:shadow-[#333333]/20 active:shadow-[#333333]">
                 <i className="fa-solid fa-forward" />
               </IconButton>
             </Link>
           </motion.div>
-          <img src="cover-letter-mock.png" alt="cover letter" className="mt-[-100px]" />
+          <img
+            src="cover-letter-mock.png"
+            alt="cover letter"
+            className="mt-[-100px]"
+          />
         </Drawer>
       </ThemeProvider>
     </>
